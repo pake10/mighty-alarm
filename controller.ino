502c8f2525d6f83d53b1dc0ecadcf63c28865f99
pake10 <pakepanda10@gmail.com>
2020-03-10 10:31:52 +0200

    Create controller.ino
    Sketched some structure of the Arduino code.

---
+++ controller.ino
@@ -0,0 +1,10 @@
+void loop() {
+  // if current state is IDLE, check the reading of the vibration sensor
+  // -> state OPEN (opening the door with the servo and bringing the figure forward)
+  // -> state WAIT (check the reading of the proximity(?) sensor and switch to CLOSE if needed)
+  // -> state CLOSE (bringing the figure back inside and closing the door)
+  // -> state IDLE
+  
+  // Will we implement the actual alarm functionality?
+  // playing the music only in state IDLE?
+}
