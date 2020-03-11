fedd56ddb05d41cc8760eab13b4b4b8b6dea2f52
pake10 <pakepanda10@gmail.com>
2020-03-11 10:45:20 +0200

    Update servos.ino
    Corrected the angle.

--- servos.ino
+++ servos.ino
@@ -1,11 +1,11 @@
 void door_control(bool open) {
   if(open) {
-    for(angle = 0; angle < 180; angle += 1) { // Open the door!
+    for(angle = 0; angle < 90; angle += 1) { // Open the door!
       door.write(angle);
       delay(5);
     }
   } else {
-    for(angle = 180; angle >= 1; angle -= 1) { // Close the door.
+    for(angle = 90; angle >= 1; angle -= 1) { // Close the door.
       door.write(angle);
       delay(5);
     }
