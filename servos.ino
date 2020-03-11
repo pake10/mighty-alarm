902355a8044694126e045399adc405fb325b3aea
pake10 <pakepanda10@gmail.com>
2020-03-11 10:13:51 +0200

    Update servos.ino
    Sketching some code for the servo control.

--- servos.ino
+++ servos.ino
@@ -1,1 +1,17 @@
+void door_control(bool open) {
+  if(open) {
+    for(angle = 0; angle < 180; angle += 1) { // Open the door!
+      door.write(angle);
+      delay(5);
+    }
+  } else {
+    for(angle = 180; angle >= 1; angle -= 1) { // Close the door.
+      door.write(angle);
+      delay(5);
+    }
+  }
+}
 
+void figure_control(bool reveal) {
+  // Reveal the figure!
+}
