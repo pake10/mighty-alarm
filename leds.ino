d56b78d65dafdca8595aaa8fce635bb068ff01f1
pake10 <pakepanda10@gmail.com>
2020-03-20 17:52:00 +0200

    Update leds.ino
    Wrote the LED controller.

--- leds.ino
+++ leds.ino
@@ -1,1 +1,25 @@
-// Controlling the leds.
+void led_control(bool reset) {
+  for(i = 0; i =< 5; i++) {
+    if (reset) {
+      digitalWrite(leds[i], LOW);
+    } else {
+      state = random(0, 2);
+    
+      switch(state) {
+        case 0: {
+          digitalWrite(leds[i], LOW);
+        }
+        
+        case 1: {
+          digitalWrite(leds[i], HIGH);
+        }
+      }
+    }
+  }
+  
+  delay(random(50, 300));
+}
+  
+   
+  
+  
