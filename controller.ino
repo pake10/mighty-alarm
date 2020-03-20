8088f54154f6f9a8bb5701d871bb209e9bf65a75
pake10 <pakepanda10@gmail.com>
2020-03-20 17:11:56 +0200

    Update controller.ino
    Added ultrasonic sensor functionality.

--- controller.ino
+++ controller.ino
@@ -1,9 +1,11 @@
 #include <SD.h>
 #include <TMRpcm.h>
 #include <SPI.h>
+#include <Servo.h>
 
 TMRpcm music;
 const int knock_threshold = 100;
+const float distance_threshold = 50.0f;
 
 const int knock_pin = A0;
 const int sd_pin = 10;
@@ -13,27 +15,49 @@ const int echo_pin = 7; // For the ultrasonic sensor
 const int door_pin = 6;
 const int figure_pin = 5;
 const int leds[6] = {2, 3, 4, 15, 16, 17}; // Analog pins are used for three LEDs.
-	
+
+Servo door;
+Servo figure;
+
 int volume = 3;
 
 enum States{IDLE, OPEN, WAIT, CLOSE, DONT_CARE};
 States state = IDLE;
 
+bool check_distance() {
+  long duration, cm;
+	
+  pinMode(trig_pin, OUTPUT);
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trig_pin, LOW);
+
+  pinMode(echo_pin, INPUT);
+  duration = pulseIn(echo_pin, HIGH);
+
+  cm = duration / 29 / 2;
+  
+  if (cm > distance_threshold) {
+    return true;
+  }
+  
+  return false;
+}
+
 void setup()
 {
   Serial.begin(9600);
-	music.speakerPin = speaker_pin;
+  music.speakerPin = speaker_pin;
   
   if (!SD.begin(sd_pin)) {  // see if the card is present and can be initialized:
     Serial.println("Couldn't initialize the SD card.");  
     return;
   }
   
-  door.attach(2);
-  figure.attach(4);
-  pinMode(7, INPUT);
-  pinMode(11, OUTPUT);
-  pinMode(3, OUTPUT);
+  door.attach(door_pin);
+  figure.attach(figure_pin);
   
   music.setVolume(volume);
   music.play("1.wav"); // Start playing the song.
@@ -42,12 +66,7 @@ void setup()
 
 void loop() {
   knock = digitalRead(knockPin);
-  // if current state is IDLE, check the reading of the vibration sensor
-  // -> state OPEN (opening the door with the servo and bringing the figure forward)
-  // -> state WAIT (check the reading of the proximity(?) sensor and switch to CLOSE if needed)
-  // -> state CLOSE (bringing the figure back inside and closing the door)
-  // -> state IDLE
-  
+
   switch(state) {
     case IDLE: {
       door.write(0); // Ensuring the door remains closed when IDLE.
