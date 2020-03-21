70769026d628af2f578dedb77079cdb5c28298f9
pake10 <pakepanda10@gmail.com>
2020-03-22 01:04:59 +0200

    Update leds.ino
    Fixed some obvious mistakes.

--- leds.ino
+++ leds.ino
@@ -1,17 +1,22 @@
 void led_control(bool reset) {
+  randomSeed(analogRead(A5)); // Seeding the random generator from an unused pin.
+  int led_state = 0;
+  
   for(i = 0; i =< 5; i++) {
     if (reset) {
       digitalWrite(leds[i], LOW);
     } else {
-      state = random(0, 2);
+      led_state = random(0, 2);
     
-      switch(state) {
+      switch(led_state) {
         case 0: {
           digitalWrite(leds[i], LOW);
+          break;
         }
         
         case 1: {
           digitalWrite(leds[i], HIGH);
+          break;
         }
       }
     }
