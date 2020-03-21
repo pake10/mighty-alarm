5bb1d41d5cfb97342b1b23a91ca05f6838b1963d
pake10 <pakepanda10@gmail.com>
2020-03-22 01:21:04 +0200

    Update controller.ino

--- controller.ino
+++ controller.ino
@@ -70,7 +70,7 @@ void loop() {
       led_control(false); // Flash the LEDs.
       door.write(0); // Ensuring the door remains closed when IDLE.
       
-      if(analogRead(knock_pin) => knock_threshold) { // Oooh, someone's at the door!
+      if(analogRead(knock_pin) >= knock_threshold) { // Oooh, someone's at the door!
         state = OPEN;
       }
       
