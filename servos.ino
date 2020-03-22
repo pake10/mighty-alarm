48153e43880c0ad42531a4a2cbb1ba87ee431836
pake10 <pakepanda10@gmail.com>
2020-03-22 02:09:12 +0200

    Update servos.ino

--- servos.ino
+++ servos.ino
@@ -1,24 +1,22 @@
 void servo_control(bool open) {
-  int angle = 0;
-  
   if(open) {
-    for(angle = 0; angle <= 90; angle += 1) { // Open the door!
+    for(int angle = 0; angle <= 90; angle += 1) { // Open the door!
       door.write(angle);
       delay(50);
     }
     
-    for(angle = 0; angle <= 90; angle += 1) { // Reveal the figure!
+    for(int angle = 0; angle <= 90; angle += 1) { // Reveal the figure!
       figure.write(angle);
       delay(50);
     }
     
   } else {
-    for(angle = 90; angle >= 0; angle -= 1) { // Hide the figure.
+    for(int angle = 90; angle >= 0; angle -= 1) { // Hide the figure.
       figure.write(angle);
       delay(50);
     }
     
-    for(angle = 90; angle >= 0; angle -= 1) { // Close the door.
+    for(int angle = 90; angle >= 0; angle -= 1) { // Close the door.
       door.write(angle);
       delay(50);
     }
